f5aef4acb921e9f19780b0ec47116e0aa49cb4c9
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-04-04 02:54:42 +0100

    converter

---
+++ JPGtoPNG.py
@@ -0,0 +1,18 @@
+# This is an exercise to create a jpg to png converter.
+# 1. create a new directory - new\ and the app should convert all the jpg from Pokedex convert to PNG.
+
+import sys
+import os
+from PIL import Image
+
+# 1. grab the second and first arguments.
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# 2. check if the new folder exist and if not create one.
+print(os.path.exists(output_folder))
+# print(image_folder, output_folder)
+
+# 3. loop through Pokedex directory
+# 4. convert images to png
+# 5. save images to the new folder
