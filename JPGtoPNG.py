232fd030069175ec62e375a43dcd3bab2cc5818b
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-04-05 00:34:55 +0100

    This is the script to check if the folder exist in a project. If the "new" folder does not exist the program will create the directory and print the result.
    Type: "python JPGtoPNG.py Pokedex\ new\" line to execute the command.

--- JPGtoPNG.py
+++ JPGtoPNG.py
@@ -1,6 +1,6 @@
 # This is an exercise to create a jpg to png converter.
 # 1. create a new directory - new\ and the app should convert all the jpg from Pokedex convert to PNG.
-
+#
 import sys
 import os
 from PIL import Image
@@ -10,7 +10,14 @@ image_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
 # 2. check if the new folder exist and if not create one.
-print(os.path.exists(output_folder))
+
+path = r"c:\Users\ncssa\PycharmProjects\imagesPython1\new"
+
+if os.path.exists(output_folder):
+    print(output_folder)
+else:
+    os.mkdir(path)
+    print(output_folder)
 # print(image_folder, output_folder)
 
 # 3. loop through Pokedex directory
